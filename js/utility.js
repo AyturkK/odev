5f710d73d861cc13a9ec32f9be8834df0207dcd9
AyturkK <ayturk.kilinc@ogr.sakarya.edu.tr>
2021-12-13 00:02:48 +0300

    Site şablonu güncellendi

---
+++ js/utility.js
@@ -0,0 +1,9 @@
+﻿
+function jumpToHeader() {
+    //window.scroll({ top: 200, behavior: 'smooth' });
+    let header = document.getElementById('main-header');
+    if (header) {
+        header.scrollIntoView({ behavior: 'smooth' });
+    }
+}
+
