bc55584b92cbfbb225015b5e72fbb27b36592278
AyturkK <ayturk.kilinc@ogr.sakarya.edu.tr>
2021-12-24 22:08:03 +0300

    Site teması ve içeriği oluşturuldu Harita kullanıcı konumu gösteriyor Sipariş sayfası eklendi Fotoğraf albümü eklendi Destek sayfası güncellendi

---
+++ js/setupmap.js
@@ -0,0 +1,47 @@
+﻿
+
+
+function initMap() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            const coordinates = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude,
+            };
+            
+            createAndAddMap(coordinates);
+            
+        });
+    }
+    
+}
+
+function createAndAddMap(coordinates) {
+    const options = {
+        zoom: 18,
+        center: coordinates,
+    };
+
+    var map = new google.maps.Map(document.getElementById("google-map-container"), options);
+
+    var marker = new google.maps.Marker({
+        position: coordinates,
+        map: map,
+    });
+
+    // API key bulana kadar...
+    setTimeout(passAlert, 100);
+    setTimeout(passAlert, 2000);
+    setTimeout(passAlert, 6000);
+
+}
+
+function passAlert() {
+    var dismissButton = document.getElementsByClassName('dismissButton')[0];
+    if (dismissButton) {
+        dismissButton.click();
+    }
+}
+
+
+
